90334803b64289aecfcae1ad5af09deec29ac0d7
Yura Kochut <kochutyurawork@gmail.com>
2020-05-08 23:25:17 +0300

    add ( navbar )

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../../services/navbar.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    public navbarS: NavbarService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from '@angular/common/http'
 
-// -----------------------
-// LIBRARIES
-// -----------------------
+//* -----------------------
+//* LIBRARIES
+//* -----------------------
 import { MatSliderModule } from '@angular/material/slider';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { DemoMaterialModule } from './modules/material.module';
 
 @NgModule({
+    declarations: [
+        NavbarComponent
+    ],
     imports: [
+        CommonModule,
         HttpClientModule,
-        MatSliderModule
+        MatSliderModule,
+        DemoMaterialModule
     ],
     exports: [
         HttpClientModule,
-        MatSliderModule
+        MatSliderModule,
+        NavbarComponent,
+        DemoMaterialModule
+    ],
+     providers: [
+        { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
     ]
 })
 
