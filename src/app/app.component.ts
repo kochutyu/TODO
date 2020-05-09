e76569ff8c9bb9c967039d7f65ff413a720fba9d
Yura Kochut <kochutyurawork@gmail.com>
2020-05-09 23:07:23 +0300

    add ( token | for auth )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavbarService } from './shared/services/navbar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(
+    private navbarS: NavbarService,
+    private router: Router
+  ) {
+    
+  }
+  ngOnInit(): void {
+    this.checkAuth();
+  }
+
+  checkAuth(): void{
+    if (localStorage.getItem('auth')) {
+      const authStatus = !!localStorage.getItem('auth');
+      this.router.navigate(['admin', 'read']);
+    }
+  }
 
 }
--- src/app/shared/services/navbar.service.ts
+++ src/app/shared/services/navbar.service.ts
@@ -4,5 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NavbarService {
+  auth: boolean;
   constructor() { }
 }
