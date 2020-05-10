76e4020587d1d95f03b6ca1cad0afad4a1646081
Yura Kochut <kochutyurawork@gmail.com>
2020-05-10 17:05:12 +0300

    add ( read | for admin )

--- src/app/admin/pages/read-page/read-page.component.ts
+++ src/app/admin/pages/read-page/read-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { TodosService } from 'src/app/shared/services/todos.service';
+import { IReadTable } from 'src/app/shared/shared.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-read-page',
   templateUrl: './read-page.component.html',
   styleUrls: ['./read-page.component.scss']
 })
-export class ReadPageComponent implements OnInit {
 
-  constructor() { }
+export class ReadPageComponent implements OnInit {
+  constructor(
+    public todoS: TodosService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.todoS.updateTodos();
+  }
+
+  edit(id: number): void{
+    this.router.navigate(['/admin', 'read', `${id}`, 'update']);
+  }
+
+  delete(id: number): void {
+    this.todoS.removeTodo(id);
+    this.todoS.updateTodos();
   }
 
 }
---
+++ src/app/shared/services/todos.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { ITodo, IReadTable } from '../shared.interface';
+import { Observable, Subscription } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosService {
+  displayedColumns: string[] = ['position', 'name', 'dateOfcreate', 'dateOfEdit', 'edit', 'delete'];
+  dataSource: IReadTable[];
+  $updateTodos: Subscription;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  updateTodos(): void {
+    let table: IReadTable[];
+    this.$updateTodos = this.getTodos().subscribe((res: ITodo[]) => {
+      this.dataSource = res.map((todo: ITodo, idx: number) => {
+        return {
+          name: todo.name,
+          dateOfcreate: new Date(todo.createdAt),
+          dateOfEdit: todo.editedAt === 'No' ? todo.editedAt : new Date(todo.editedAt),
+          edit: todo.id,
+          delete: todo.id,
+          position: idx
+        }
+      })
+      this.$updateTodos.unsubscribe();
+    }, err => { console.log(err) })
+  }
+
+  getTodos(): Observable<ITodo[]> {
+    return this.http.get<ITodo[]>('http://localhost:3000/todos');
+  }
+
+  removeTodo(id: number): void {
+    let $delete: Subscription;
+    $delete = this.http.delete(`http://localhost:3000/todos/${id}`).subscribe(res => {
+      $delete.unsubscribe();
+    }, err => {
+        console.log(err);
+    })
+  }
+}
