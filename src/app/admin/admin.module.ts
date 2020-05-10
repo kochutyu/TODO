93bdfc7953aeee408b1f070f04b92b0c55e47b53
Yura Kochut <kochutyurawork@gmail.com>
2020-05-10 21:55:10 +0300

    remove ( extra components )

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,9 +8,7 @@ import { RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
 import { ReadPageComponent } from './pages/read-page/read-page.component';
-import { DeletePageComponent } from './pages/delete-page/delete-page.component';
 import { UpdatePageComponent } from './pages/update-page/update-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { AuthService } from './shared/services/auth.service';
 import { ModalService } from '../shared/services/modal.service';
@@ -20,9 +18,7 @@ import { AuthGuardService } from './shared/services/auth-guard.service';
     AdminLayoutComponent,
     CreatePageComponent,
     ReadPageComponent,
-    DeletePageComponent,
     UpdatePageComponent,
-    LoginPageComponent,
   ],
   imports: [
     CommonModule,
@@ -31,10 +27,8 @@ import { AuthGuardService } from './shared/services/auth-guard.service';
       {
         path: '', component: AdminLayoutComponent, children: [
           { path: '', redirectTo: '/admin/read', pathMatch: 'full' },
-          { path: 'login', component: LoginPageComponent, canActivate: [AuthGuardService]},
           { path: 'create', component: CreatePageComponent, canActivate: [AuthGuardService]},
           { path: 'read', component: ReadPageComponent, canActivate: [AuthGuardService]},
-          { path: 'delete', component: DeletePageComponent, canActivate: [AuthGuardService]},
           { path: 'read/update/:id', component: UpdatePageComponent, canActivate: [AuthGuardService] },
           { path: '**', redirectTo: '/admin/read'}
         ]
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ReadPageComponent } from './pages/read-page/read-page.component';
 import { ShowTodoComponent } from './pages/show-todo/show-todo.component';
 
