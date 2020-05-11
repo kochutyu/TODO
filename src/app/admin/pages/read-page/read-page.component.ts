d60d7ee21ecfd76fbc95b038b2a2c622b56a5779
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 17:36:11 +0300

    update ( read-page | optimize code )

--- src/app/admin/pages/read-page/read-page.component.ts
+++ src/app/admin/pages/read-page/read-page.component.ts
@@ -10,24 +10,40 @@ import { ModalService } from 'src/app/shared/services/modal.service';
 })
 
 export class ReadPageComponent implements OnInit {
+
   constructor(
     public todoS: TodosService,
     private router: Router,
     private modalS: ModalService
   ) { }
 
+
+
+
+
   ngOnInit() {
     this.todoS.updateTodos();
   }
 
+
+
+
+
+  // EDIT TODO
   edit(id: number): void{
     this.router.navigate(['/admin', 'read', 'update', `${id}`]);
   }
 
+
+
+
+
+  // DELETE TODO
   delete(id: number): void {
     this.modalS.openConfirm();
     this.todoS.removeID = id;
+
+    // GET BTN FOR CONFIRM DELETE TODO
     this.modalS.confirmDeleteTodo = true;
   }
-
 }
