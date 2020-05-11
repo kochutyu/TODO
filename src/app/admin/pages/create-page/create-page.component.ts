2e94169ea55e3cd09092a11515ce5863b0fe943b
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 17:17:41 +0300

    update ( create-page | optimize code )

--- src/app/admin/pages/create-page/create-page.component.ts
+++ src/app/admin/pages/create-page/create-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { CreatePageService } from '../../shared/services/pages/create-page.service';
-import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,20 +10,51 @@ import { ModalService } from 'src/app/shared/services/modal.service';
 export class CreatePageComponent implements OnInit {
 
   form: FormGroup;
+
+
+
+
+
+  
   constructor(
     public createPageS: CreatePageService
   ) { }
 
+
+
+
+
+
+
   ngOnInit() {
-    this.createPageS.name = this.createPageS.description = '';
+    this.initForm();
+  }
+  
+
+
+
+
+  initForm(): void{
+    // START VALUE FOR FORM
+    this.createPageS.resetData();
+
+    // GROUP FROM
     this.form = new FormGroup({
       name: new FormControl(this.createPageS.name, [Validators.required]),
       description: new FormControl(this.createPageS.description, [Validators.required]),
     });
+
+    // SAVE FORM IN SERVICE FOR RESPONSIVE RESET FORM
     this.createPageS.form = this.form;
   }
 
+
+
+
+
   submit(): void{
+
+    // CREATE TODO
     this.createPageS.setTodo();
   }
 
