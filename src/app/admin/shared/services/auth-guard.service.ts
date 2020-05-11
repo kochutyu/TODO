9b8db544cb18455a99f2bf2070fd612c8a099595
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 17:59:01 +0300

    update ( auth-guard | optimize code )

--- src/app/admin/shared/services/auth-guard.service.ts
+++ src/app/admin/shared/services/auth-guard.service.ts
@@ -9,12 +9,21 @@ export class AuthGuardService implements CanActivate{
   constructor(
     private router: Router
   ) { }
+
+
+
+
+
   canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+    
+    // GET KEY FROM LOCALSTORAGE FOR AUDIT FOR AUTHORIZATION
     const authStatus = localStorage.getItem('auth');
-    if (!authStatus) {
+
+    if (!authStatus) { // IF LOCAL STORAGE HAVE NOT KEY
       this.router.navigate(['/']);
       return false
-    } else {
+    } else { // IF LOCAL STORAGE HAVE KEY
+
       return true
     }
   }
