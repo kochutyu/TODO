2f7d150c00285cc89c911c2b2651a6f32150c931
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 19:21:29 +0300

    update ( app.module | optimize code )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//* -----------------------
+//* DECLARATION
+//* -----------------------
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { ReadPageComponent } from './pages/read-page/read-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
 import { ShowTodoComponent } from './pages/show-todo/show-todo.component';
+
+//* -----------------------
+//* IMPORTS
+//* -----------------------
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
 
 @NgModule({
   declarations: [
