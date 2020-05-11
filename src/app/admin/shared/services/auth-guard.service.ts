e4243de23c4f186aacc4da3d9d35b544bde91acc
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 17:06:47 +0300

    update ( routing | for all modules )

--- src/app/admin/shared/services/auth-guard.service.ts
+++ src/app/admin/shared/services/auth-guard.service.ts
@@ -10,11 +10,12 @@ export class AuthGuardService implements CanActivate{
     private router: Router
   ) { }
   canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-    const authStatus = !!localStorage.getItem('auth');
+    const authStatus = localStorage.getItem('auth');
     if (!authStatus) {
       this.router.navigate(['/']);
       return false
+    } else {
+      return true
     }
-    return true
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import { ReadPageComponent } from './pages/read-page/read-page.component';
 import { ShowTodoComponent } from './pages/show-todo/show-todo.component';
+import { AuthGuardService } from './admin/shared/services/auth-guard.service';
 
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
-      {path: '', redirectTo: '/read', pathMatch: 'full'},
+      { path: '', redirectTo: '/read', pathMatch: 'full' },
       { path: 'read', component: ReadPageComponent },
       { path: 'read/:id', component: ShowTodoComponent },
     ]
@@ -16,6 +17,7 @@ const routes: Routes = [
   {
     path: 'admin', loadChildren: './admin/admin.module#AdminModule'
   },
+  { path: '**', redirectTo: '/read', canActivate: [!AuthGuardService]}
 ];
 
 @NgModule({
