01fcb94f5be77cd27590fe0d0c81837177b70f5f
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 18:10:04 +0300

    update ( auth | optimize code )

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IAdmin } from 'src/app/shared/shared.interface';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Admin } from 'src/app/shared/shared.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
@@ -12,6 +12,8 @@ import { NavbarService } from 'src/app/shared/services/navbar.service';
 })
 export class AuthService {
 
+  $admin: Subscription;
+
   constructor(
     private http: HttpClient,
     private _snackBar: MatSnackBar,
@@ -19,47 +21,77 @@ export class AuthService {
     private navbarS: NavbarService
   ) { }
 
-  checkLogin(email:string, password:string): void {
-    this.getAdminInfo().subscribe((res: IAdmin) => {
-      
-      const admin = new Admin(email, password);
 
-      if (admin.email === res.email && admin.password === res.password) {
 
-        this.setToken(admin);
-        this.navbarS.authStatus = true;
-        this.router.navigate(['/admin', 'read']);
-      } else {
-
-        this.setErrorMessage();
-      }
-    });
-  }
-
-  logOut(): void{
-    if (localStorage.getItem('auth')) {
-      this.removeToken();
-      this.navbarS.authStatus = false;
-      this.router.navigate(['/']);
-    }
-  }
 
 
   private getAdminInfo(): Observable<IAdmin> {
     return this.http.get<IAdmin>(' http://localhost:3000/admin');
   }
 
+
+
+
+
   private setToken(admin: Admin): void{
     localStorage.setItem('auth', JSON.stringify(true))
   }
+
+
+
+
 
   private removeToken(): void{
     localStorage.removeItem('auth');
   }
 
+
+
+
+
   private setErrorMessage(): void{
     this._snackBar.open('You are not admin!', "Please try logging in again!", {
       duration: 3000,
     });
+  }
+
+
+
+
+
+  checkLogin(email: string, password: string): void {
+
+    this.$admin = this.getAdminInfo().subscribe((res: IAdmin) => {
+
+      const admin = new Admin(email, password);
+
+      if (admin.email === res.email && admin.password === res.password) { // CONDITION FOR LOGIN BY ADMIN
+
+        this.setToken(admin);
+        this.navbarS.authStatus = true;
+        this.router.navigate(['/admin', 'read']);
+      } else { // MISTAKE OF DATA FOR USER
+
+        this.setErrorMessage();
+      }
+
+      this.$admin.unsubscribe();
+    });
+  }
+
+
+
+
+
+
+  logOut(): void {
+    if (localStorage.getItem('auth')) { // IF ADMIN IS AUTHORISED
+
+      // SHOW NAVBAR FOR USER
+      this.navbarS.authStatus = false;
+
+      this.router.navigate(['/']);
+      this.removeToken();
+    }
   }
 }
