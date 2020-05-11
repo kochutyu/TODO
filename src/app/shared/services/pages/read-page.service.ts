7618aa589a1ef5fb1b980d00d9f969243091d376
Yura Kochut <kochutyurawork@gmail.com>
2020-05-11 15:52:32 +0300

    update ( show-todo | route )

--- src/app/shared/services/pages/read-page.service.ts
+++ src/app/shared/services/pages/read-page.service.ts
@@ -3,11 +3,13 @@ import { ITodo } from '../../shared.interface';
 import { Router } from '@angular/router';
 import { ShowTodoService } from './show-todo.service';
 import { TodosService } from '../todos.service';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReadPageService {
+  $todos: Subscription;
 
   constructor(
     private router: Router,
@@ -16,13 +18,27 @@ export class ReadPageService {
   ) { }
 
   readTodo(id: number): void {
-  
-    this.todoS.getTodo(id).subscribe((todo: ITodo) => {
-      this.showTodoS.todo = todo;
-      this.showTodoS.statusContentInit = true;
-    })
 
-    this.router.navigate(['/read', id]);
+    if (isNaN(id)) { // if id is NaN
+
+      this.router.navigate(['/read']);
+    } else { // if id is number
+      this.$todos = this.todoS.getTodos().subscribe((todos: ITodo[]) => {
+        this.showTodoS.todo = todos.find((todo: ITodo) => todo.id === id);
+
+        if (this.showTodoS.todo) { // if id exist
+
+          this.showTodoS.statusContentInit = true;
+          this.router.navigate(['/read', id]);
+        } else { // if id not exist
+
+          this.router.navigate(['/read']);
+        }
+
+        this.$todos.unsubscribe(); // unsubscribe
+      })
+    }
+
   }
 
 }
